1fe8775f6fa10f4b098c07ebb79e57c7fea376bf
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-08 16:22:45 +0300

    02

---
+++ src/Components/Profile/ProfileItems/ProfileItems.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function ProfileItems() {
+    return (
+        <img
+            src='https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-05.jpg'/>
+    )
+}
