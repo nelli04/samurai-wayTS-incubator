520aeda24c6673fa297d5b53620535e0691af5fe
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-08 16:22:34 +0300

    02

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import prof from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
-
-
+import {ProfileItems} from "./ProfileItems/ProfileItems";
 
 export function Profile () {
     return (
         <div className={prof.content}>
-            <img
-                src='https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-05.jpg'/>
+            <ProfileItems/>
             <MyPosts/>
         </div>
     )
