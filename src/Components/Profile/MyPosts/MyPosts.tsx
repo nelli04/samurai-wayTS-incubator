b9f8330f278da68bdaba94644e34d226e2565805
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-18 11:06:47 +0300

    03

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -3,13 +3,27 @@ import myposts from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 
 export function MyPosts () {
+
+    let postData = [
+        {
+            id: 1,
+            message: 'Hey He',
+            likeCount: 12
+        },
+        {
+            id: 2,
+            message: 'Hey Hey friends',
+            likeCount: 10
+        }
+    ]
+    const postHandler = postData.map( p =>  <Post message={p.message} likesCount={p.likeCount}/> )
+
     return (
         <div className={myposts.myposts}>
             <textarea></textarea>
             <button>Отправить</button>
             <div className={myposts.post}>
-                <Post message='Hey He'/>
-                <Post message='Hey Hey friends'/>
+                { postHandler }
             </div>
 
         </div>
