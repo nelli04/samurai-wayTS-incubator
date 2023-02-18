9147e543bd4a73f28e45514d22ed1d66c0796af0
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-18 11:06:20 +0300

    03

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -4,21 +4,56 @@ import dialog from "./DialogItem/DialogsItem.module.css";
 import {Message} from "./Message/Message";
 
 export function Dialogs() {
+
+    let dialogsData = [
+        {
+            id: 'User1',
+            name: 'User1'
+        },
+        {
+            id: 'User2',
+            name: 'User2'
+        },
+        {
+            id: 'User3',
+            name: 'User3'
+        },
+        {
+            id: 'User4',
+            name: 'User4'
+        },
+    ]
+    let messagesData = [
+        {
+            id: 1,
+            message: 'Hello'
+        },
+        {
+            id: 2,
+            message: 'Hello'
+        },
+        {
+            id: 3,
+            message: 'Hello'
+        },
+        {
+            id: 4,
+            message: 'Hello'
+        },
+    ]
+    const mapDialogDataHandler = dialogsData.map(d => <DialogItem name={d.name} id={d.id}/>)
+    const  mapMessagesDataHandler = messagesData.map(m => <Message message={m.message} id={m.id}/>)
+
+    debugger;
+
     return (
         <div className={dialog.dialogs}>
             <div className={dialog.dialog_item}>
-                <DialogItem name = 'User1' id='User1'/>
-                <DialogItem name = 'User2' id='User2'/>
-                <DialogItem name = 'User3' id='User3'/>
-                <DialogItem name = 'User4' id='User4'/>
+                { mapDialogDataHandler }
             </div>
             <div className={dialog.messages}>
-                <Message message='Hello'/>
-                <Message message='Hello'/>
-                <Message message='Hello'/>
-                <Message message='Hello'/>
+                { mapMessagesDataHandler }
             </div>
         </div>
     )
 }
-
