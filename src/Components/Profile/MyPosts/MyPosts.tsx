096d10e0377336e0f96bb9950ef8a7faf9e44aa0
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-18 15:27:52 +0300

    03

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import myposts from "./MyPosts.module.css";
+import myPosts from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 import {DataType} from "../../../index";
 
@@ -8,10 +8,19 @@ export function MyPosts(props: DataType) {
     const postHandler = props.postData.map(p => <Post message={p.message} likesCount={p.likeCount}/>)
 
     return (
-        <div className={myposts.myposts}>
-            <textarea></textarea>
-            <button>Отправить</button>
-            <div className={myposts.post}>
+        <div >
+            <div className={myPosts.myposts}>
+                <h3>My Posts</h3>
+            </div>
+
+            <div className={myPosts.text}>
+                <textarea></textarea>
+            </div>
+            <div className={myPosts.button}>
+                <button>Отправить</button>
+            </div>
+
+            <div className={myPosts.post}>
                 {postHandler}
             </div>
 
