39f23a81d1a428d17a0b3f064cddab7ac33c8405
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-18 15:30:49 +0300

    03

--- src/Components/Profile/ProfileItems/ProfileItems.tsx
+++ src/Components/Profile/ProfileItems/ProfileItems.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import propfileItem from './ProfileItems.module.css'
 
 export function ProfileItems() {
     return (
-        <img src='https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-05.jpg' alt=''/>
+        <div>
+            <div className={propfileItem.container}>
+                <img className={propfileItem.img}
+                     src='https://img.freepik.com/premium-vector/doodle-face-avatar-for-social-media-line-art-people-face-isolated-on-white-background_559729-359.jpg'
+                     alt=''/>
+                <div className={propfileItem.overlay}>
+                    @username
+                </div>
+            </div>
+            <div className={propfileItem.ava}>
+                @username
+            </div>
+        </div>
+
+
     )
 }
