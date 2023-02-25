79db7213e68c673d3c78fc217aaf3f20a15ff9e0
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-25 18:04:26 +0300

    03

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import myPosts from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
-import {DataType} from "../../../state/state";
+import {DataType} from "../state";
 
 export function MyPosts(props: DataType) {
 
     const postHandler = props.profile.postData.map(p => <Post message={p.message} likesCount={p.likeCount}/>)
+    const newPostElement = React.createRef<HTMLTextAreaElement>()
+    const onClickButtonHandler = () => {
+        let text = newPostElement.current?.value
+    }
 
     return (
-        <div >
+        <div>
             <div className={myPosts.myposts}>
                 <h3>My Posts</h3>
             </div>
 
             <div className={myPosts.text}>
-                <textarea></textarea>
+                <textarea ref={newPostElement}></textarea>
             </div>
             <div className={myPosts.button}>
-                <button>Отправить</button>
+                <button onClick={onClickButtonHandler}>Отправить</button>
             </div>
 
             <div className={myPosts.post}>
