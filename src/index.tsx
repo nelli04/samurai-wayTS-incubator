462c86be0f83c9b5683351556526429787d2e5c6
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-25 18:04:09 +0300

    03

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {state} from "./state/state";
+import {state} from "./Components/Profile/state";
 
 ReactDOM.render(
     <BrowserRouter>
