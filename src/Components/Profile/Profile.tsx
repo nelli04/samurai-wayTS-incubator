41c5b5db1658960a99ffab98267215d3e9035a29
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-25 18:04:50 +0300

    03

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import prof from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileItems} from "./ProfileItems/ProfileItems";
-import {DataType} from "../../state/state";
+import {DataType} from "./state";
 
 
 export function Profile (props: DataType) {
