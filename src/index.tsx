97510d3731214b7d14de061067cbabbd46053ef9
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-26 15:19:39 +0300

    03

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import './index.css';
+import {rerenderEntireTree} from "./render";
+import {state} from "./Components/Profile/state";
+/*
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {state} from "./Components/Profile/state";
+*/
 
+rerenderEntireTree(state)
+
+/*
 ReactDOM.render(
     <BrowserRouter>
         <App state={state}/>
     </BrowserRouter>,
     document.getElementById('root')
-);
+);*/
