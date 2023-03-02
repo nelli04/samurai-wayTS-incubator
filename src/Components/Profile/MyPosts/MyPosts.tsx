2023aee668012b50181e5844a0a733a0029cfacf
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-02 11:19:32 +0300

    05

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import myPosts from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
-import {DataType, PostDataType} from "../state";
+import {DataType} from "../state";
 
-export type MyPostsType = {
+/*export type MyPostsType = {
     postData: PostDataType[]
     addPost: (postMessage: string) => void
-}
+}*/
 
 export function MyPosts(props: DataType) {
 
-    const postHandler = props.profile.postData.map(p => <Post message={p.message} likesCount={p.likeCount}/>)
-    const newPostText = React.createRef<HTMLTextAreaElement>()
+    const postHandler = props.profile.postData.map(p => <Post key={p.id} message={p.message} likesCount={p.likeCount}/>)
+    //const newPostText = React.createRef<HTMLTextAreaElement>()
     const onClickButtonHandler = () => {
-        props.addPost(newPostText.current ? newPostText.current.value : '')
+        props.addPost(props.profile.newPostMessage/*newPostText.current ? newPostText.current.value : ''*/)
+    }
+    const onChangeTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        props.changeNewText(e.currentTarget.value)
     }
 
     return (
         <div>
-            <div className={myPosts.myposts}>
+            <div className={myPosts.my_posts}>
                 <h3>My Posts</h3>
             </div>
 
             <div className={myPosts.text}>
-                <textarea ref={newPostText}></textarea>
+                <textarea /*ref={newPostText}*/
+                          onChange={onChangeTextarea}
+                          value={props.profile.newPostMessage}
+                />
             </div>
             <div className={myPosts.button}>
                 <button onClick={onClickButtonHandler}>Отправить</button>
