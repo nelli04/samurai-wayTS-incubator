79343b025f0b15d9245546aa885cf4fd9502edea
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-02 12:56:05 +0300

    05

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
-import {rerenderEntireTree} from "./render";
-import {state} from "./Components/Profile/state";
-/*
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {state} from "./Components/Profile/state";
-*/
+import {state, subscribe} from "./Components/Profile/state";
 
-rerenderEntireTree(state)
 
-/*
-ReactDOM.render(
-    <BrowserRouter>
-        <App state={state}/>
-    </BrowserRouter>,
-    document.getElementById('root')
-);*/
+export let rerenderEntireTree = (/*state: DataType*/) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
+rerenderEntireTree()
+subscribe(rerenderEntireTree)
+
+
+
